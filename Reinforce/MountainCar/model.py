2faceacbccba508c085f5118db17c7a46a208f96
collinsodhiambo <collinsodhiambo8290@gmail.com>
2024-11-14 19:52:22 +0300

    Added some files
    Added implementation for Reinforce and model checkpoints

---
+++ Reinforce/MountainCar/model.py
@@ -0,0 +1,31 @@
+from torch import nn
+import torch
+from torch.nn import functional as F
+from torch.distributions import Categorical
+
+class Policy(nn.Module):
+    def __init__(self, input_dims = 4, hidden_dim = 16, n_actions = 4, device = 'cuda', seed = 42):
+        super().__init__()
+
+        torch.manual_seed(seed)
+        self.fc1 = nn.Linear(input_dims, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim * 2)
+        self.fc3 = nn.Linear(hidden_dim * 2, n_actions)
+
+        self.device = torch.device(device)
+        self.to(self.device)
+
+    def forward(self, state):
+        # convert the state to a torch tensor
+        state = torch.FloatTensor(state, device=self.device).unsqueeze(0)
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = F.softmax(self.fc3(x), dim=1)
+        return x
+
+    def act(self, state):
+        x = self(state).cpu()
+        dist = Categorical(x)
+        action = dist.sample()
+        log_prob = dist.log_prob(action)
+        return action.item(), log_prob
---
+++ Reinforce/MountainCar/train.py
@@ -0,0 +1,93 @@
+import gymnasium as gym
+import torch
+import numpy as np
+from model import Policy as PG
+from torch.optim import Adam
+from collections import deque
+import matplotlib.pyplot as plt
+
+seed = 42
+torch.manual_seed(seed)
+
+name = 'MountainCar-v0'
+env = gym.make(name)#, render_mode = 'human')
+threshold = gym.envs.registry.get(name).reward_threshold
+max_iter = gym.envs.registry.get(name).max_episode_steps
+
+env.reset(seed=seed)
+n_actions = env.action_space.n
+input_dim = env.observation_space.shape[0]
+hidden_dim = 64
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+lr = 1e-3
+gamma = 0.99
+
+net = PG(input_dims=input_dim, hidden_dim=hidden_dim,
+         n_actions=n_actions,device=device, seed=seed)
+
+optimizer = Adam(net.parameters(),lr=lr)
+
+def train(episodes = 2000, max_iter = 1000):
+    scores = []
+    scores_window = deque(maxlen=100)
+
+
+    for episode_idx in range(1, episodes + 1):
+        log_probs = []
+        rewards = []
+        score = 0.0
+
+        state, _ = env.reset()
+        for i in range(max_iter):
+            action, log_prob = net.act(state)
+            state, reward, done, trunc, _ = env.step(action)
+            log_probs.append(log_prob)
+            rewards.append(reward)
+            score += reward
+            if done or trunc:
+                break
+        
+        scores.append(score)
+        scores_window.append(score)
+
+        discounted_rewards = []
+        for t in range(len(rewards)):
+            Gt = 0
+            for i, r in enumerate(rewards[t:]):
+                Gt += r * gamma ** i
+            discounted_rewards.append(Gt)
+            
+        discounted_rewards =  torch.tensor(discounted_rewards)
+        discounted_rewards = (discounted_rewards - discounted_rewards.mean())/ (discounted_rewards.std() + 1e-9)
+
+        grad_log_probs_list = []
+        for log_prob, R in zip(log_probs, discounted_rewards):
+            grad_log_probs_list.append(-log_prob * R)
+
+        grads = torch.cat(grad_log_probs_list).sum()
+        optimizer.zero_grad()
+        grads.backward()
+        optimizer.step()
+
+        if episode_idx % 100 == 0:
+            print(f"Episode: {episode_idx}\tAverage Score: {np.mean(scores_window)}")
+        
+        if np.mean(scores_window) >= threshold:
+            print(f"Episode: {episode_idx}\tAverage Score: {np.mean(scores_window)}")
+            print(f"Environment solved in {episode_idx} steps! Mean score: {np.mean(scores_window)}")
+
+            data = {
+                'model_state_dict':net.state_dict()
+            }
+            torch.save(data, f"Lunar_lander_reinforce_{episode_idx}_steps.pt")
+            break
+
+    return scores
+
+scores = train(max_iter=max_iter)
+
+plt.plot(np.arange(len(scores)), scores)
+plt.xlabel("# Episodes")
+plt.ylabel("Scores")
+plt.savefig("Mountain_car_reinforce_results.png")
+plt.show()
